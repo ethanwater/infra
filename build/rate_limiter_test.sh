192ea97769b775fe8f4d6e05340bfae0dda1d450
Ethan Water <watermonic@gmail.com>
2024-01-24 00:04:53 -0500

    fix/action++

--- build/rate_limiter_test.sh
+++ build/rate_limiter_test.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
-
 counter=0
 
 while [ "$counter" -lt 15 ]
 do
-	url="http://127.0.0.1:8080/bella/2FA?action=generate"
-	curl "$url"
+    url="http://127.0.0.1:8080/bella/2FA?action=generate"
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
-	((counter++))
+    if [ "$response" -ne 200 ]; then
+        echo "Request failed with status code $response. Exiting script."
+        break
+    fi
+
+    ((counter++))
 done
