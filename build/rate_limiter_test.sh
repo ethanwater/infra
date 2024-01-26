60b0778e5cba9906854b4bb7a7aba3605be1cc0c
Ethan Water <watermonic@gmail.com>
2024-01-25 19:58:56 -0500

    actions/disable rate-limiter test

--- build/rate_limiter_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do
-	url="http://127.0.0.1:8080/bella/2FA?action=generate"
-	curl "$url"
-done
---
+++ scripts/rate_limit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#while true
+#do
+#	url="http://127.0.0.1:8080/bella/2FA?action=generate"
+#	curl "$url"
+#done
